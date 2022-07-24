4eaf84ec84e23051dfc54246b750a9bd197d219b
Andy0514 <wanghaolinsh@icloud.com>
2022-07-24 00:11:59 -0700

    Add files via upload

---
+++ aiMove/obj/default/hello_world.d
@@ -0,0 +1,14 @@
+obj/default/hello_world.o: hello_world.c ../aiMove_bsp/system.h \
+ ../aiMove_bsp/linker.h \
+ ../aiMove_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../aiMove_bsp//HAL/inc/io.h ../aiMove_bsp//HAL/inc/alt_types.h
+
+../aiMove_bsp/system.h:
+
+../aiMove_bsp/linker.h:
+
+../aiMove_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../aiMove_bsp//HAL/inc/io.h:
+
+../aiMove_bsp//HAL/inc/alt_types.h:
